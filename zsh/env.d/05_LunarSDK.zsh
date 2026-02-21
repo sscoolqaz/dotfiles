f4ad33774f41d5909591f55f9aa04d85dbbb5adc
Sierra S <sscoolqaz@protonmail.com>
2026-02-20 21:00:26 -0600

    adjusted LunarSDK logic so it works properly

--- zsh/env.d/05_LunarSDK.zsh
+++ zsh/env.d/05_LunarSDK.zsh
@@ -1,11 +1,13 @@
-setopt extendedglob glob_assign
+setopt extendedglob
 
 if [[ -d ~/dev/LunarG_VK ]]; then
     
-    export VULKAN_SDK=~/dev/LunarG_VK/1.<->.<->.<->/x86_64(N:on[1])/
+    latest_dir=$(print -rl ~/dev/LunarG_VK/1.*.*.*(N) | sort -V | tail -n 1)
+
+    export VULKAN_SDK="$latest_dir/x86_64"
+
     export PATH=$VULKAN_SDK/bin:$PATH
     export LD_LIBRARY_PATH=$VULKAN_SDK/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
     export VK_ADD_LAYER_PATH=$VULKAN_SDK/share/vulkan/explicit_layer.d
     export PKG_CONFIG_PATH=$VULKAN_SDK/share/pkgconfig:$VULKAN_SDK/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
-
 fi
