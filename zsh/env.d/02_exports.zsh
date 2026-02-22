383ab32956bff19064f498190dc4bbf67d86a3c2
Sierra S <sscoolqaz@protonmail.com>
2026-02-21 20:53:11 -0600

    Added Gradle and Java Support

--- zsh/env.d/02_exports.zsh
+++ zsh/env.d/02_exports.zsh
@@ -57,3 +57,6 @@ export PERL_CPANM_HOME=$XDG_CACHE_HOME/cpanm
 export SOLARGRAPH_CACHE=$XDG_CACHE_HOME/solargraph
 export GTK2_RC_FILES=$XDG_CONFIG_HOME/gtk-2.0/gtkrc
 export CARGO_HOME=$XDG_DATA_HOME/cargo
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+
