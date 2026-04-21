a79f8429ec8202e6a1a9b4484e4c77248ee04674
Sierra S <sscoolqaz@protonmail.com>
2026-04-20 20:46:59 -0500

    adjusted nvim colorscheme to catpuccin :3

--- nvim/init/10_colorscheme.lua
+++ nvim/init/10_colorscheme.lua
@@ -1,35 +1,31 @@
-require('solarized').setup({
-  variant = 'autumn',
-  transparent = {
-      enabled = true,         -- Master switch to enable transparency
-      pmenu = true,           -- Popup menu (e.g., autocomplete suggestions)
-      normal = true,          -- Main editor window background
-      normalfloat = true,     -- Floating windows
-      neotree = true,         -- Neo-tree file explorer
-      nvimtree = true,        -- Nvim-tree file explorer
-      whichkey = true,        -- Which-key popup
-      telescope = true,       -- Telescope fuzzy finder
-      lazy = true,            -- Lazy plugin manager UI
-      mason = true,           -- Mason manage external tooling
+require('catppuccin').setup({
+  flavour = 'frappe',
+  background = {
+    light = 'latte',
+    dark = 'mocha',
   },
-  on_highlights = function(colors, helper)
-    return {
-      -- undercurl the spelling errors
-      SpellBad = { strikethrough = false, underline = false, sp = colors.red, undercurl = true },
-      SpellCap = { sp = colors.violet, undercurl = true },
-      SpellLocal = { sp = colors.yellow, undercurl = true },
-      SpellRare = { sp = colors.cyan, undercurl = true },
-      -- better mini.diff readability
-      MiniDiffOverAdd = { fg = colors.git_add },
-      MiniDiffOverChange = { fg = helper.blend(colors.git_modify, colors.git_delete, 0.5) },
-      MiniDiffOverChangeBuf = { fg = helper.blend(colors.git_modify, colors.git_add, 0.5) },
-      MiniDiffOverContext = { fg = colors.git_delete },
-      MiniDiffOverContextBuf = { fg = colors.git_add },
-      MiniDiffOverDelete = { fg = colors.git_delete },
-      -- proper mini.indentscope highlight
-      MiniIndentscopeSymbol = { fg = colors.base01 },
-    }
-  end,
+  transparent_background = true,
+  float = {
+    transparent = false,
+    solid = true,
+  },
+  dim_inactive = {
+    enabled = true,
+    shade = 'dark',
+    percentage = 0.15,
+  },
+  default_integrations = true,
+  auto_integrations = false,
+  integrations = {
+    blink_cmp = true,
+    nvimtree = true,
+    markview = false,
+    mason = false,
+    mini = {
+      enabled = true,
+      indentscope_color = '',
+    },
+  },
 })
 
-vim.cmd.colorscheme('solarized')
+vim.cmd.colorscheme('catppuccin-nvim')
